62f008554665df99ccbad1d267b44a17c6de2fee
Sebastian Weddmark Olsson <visnae@gmail.com>
2026-05-10 17:57:42 +0200

    refactor eunit tests; rename, uniform, and speed up

--- test/redbug_eunit.erl
+++ test/redbug_eunit.erl
@@ -5,176 +5,134 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-t_0_test() ->
-  Filename = "redbug0.txt",
-  {_, _, _} = redbug:start("lists:sort", [{print_file, Filename}, print_msec, debug]),
+millisecond_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort", [print_msec]),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
-  TS = re:split(get_line_seg(Filename, 1, 2), "[:.]"),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
+  Timestamp = get_line_seg(Content, 1, 2),
+  Re = re:run(Timestamp, "(.*)[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}(.*)", [{capture, all}]),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
-  ?assertEqual(4,
-               length(TS)),
-  ?assertEqual(3,
-               size(lists:last(TS))),
-  maybe_delete(Filename).
+               get_line_seg(Content, 2, 2)),
+  ?assertEqual({match, [{0,12}, {0,0}, {12,0}]}, Re).
 
-t_01_test() ->
-  Filename = "redbug01.txt",
-  {_, _, _} = redbug:start("lists:sort", [{print_file, Filename}, arity, debug]),
+arity_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort", [arity]),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"lists:sort/1">>,
-               get_line_seg(Filename, 2, 2)),
+               get_line_seg(Content, 2, 2)),
   ?assertEqual(3,
-               length(re:split(get_line_seg(Filename, 1, 2), "[:.]"))),
-  maybe_delete(Filename).
+               length(re:split(get_line_seg(Content, 1, 2), "[:.]"))).
 
-t_02_test() ->
-  Filename = "redbug02.txt",
-  {_, _, _} = redbug:start("lists:sort", [{print_file, Filename}, {print_time_unit, microsecond}, debug]),
+microsecond_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort", [{print_time_unit, microsecond}]),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
-  TS = re:split(get_line_seg(Filename, 1, 2),"[:.]"),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
+  Timestamp = get_line_seg(Content, 1, 2),
+  Re = re:run(Timestamp, "(.*)[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6}(.*)", [{capture, all}]),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
-  ?assertEqual(4,
-               length(TS)),
-  ?assertEqual(6,
-               size(lists:last(TS))),
-  maybe_delete(Filename).
+               get_line_seg(Content, 2, 2)),
+  ?assertEqual({match, [{0,15}, {0,0}, {15,0}]}, Re).
 
-t_1_test() ->
-  Filename = "redbug1.txt",
-  {_, _, _} = redbug:start("lists:sort->return", [{print_file, Filename}, buffered, debug]),
+buffered_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort->return", [buffered]),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
+               get_line_seg(Content, 2, 2)),
   ?assertEqual([<<"lists:sort/1">>, <<"->">>, <<"[1,2,3]">>],
-               get_line_seg(Filename, 4, 2, 4)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 4, 2, 4)).
 
-t_2_test() ->
-  Filename = "redbug2.txt",
-  {_, _, _} = redbug:start("lists:sort->stack", [{print_file, Filename}, debug]),
+return_stack_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort->stack", []),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
-  Lines = lists:seq(3, lines(Filename)-1),
+               get_line_seg(Content, 2, 2)),
+  Lines = lists:seq(3, lines(Content)-1),
   ?assertEqual([true],
-               lists:usort([is_mfa(get_line_seg(Filename, L, 2))||L<-Lines])),
-  maybe_delete(Filename).
+               lists:usort([is_mfa(get_line_seg(Content, L, 2))||L<-Lines])).
 
-t_3_test() ->
-  Filename = "redbug3.txt",
+proc_send_test() ->
   Pid = spawn(fun()->receive P when is_pid(P)->P!ding;quit->ok end end),
-  {_, _, _} = redbug:start(send, [{procs, Pid}, {print_file, Filename}, debug]),
+  redbug_start(?FUNCTION_NAME, send, [{procs, Pid}]),
   Pid ! self(),
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"ding">>,
-               get_line_seg(Filename, 2, 4)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 2, 4)).
 
-t_4_test() ->
-  Filename = "redbug4.txt",
+proc_receive_test() ->
   Pid = spawn(fun()->receive P when is_pid(P)->P!ding;quit->ok end end),
-  {_, _, _} = redbug:start('receive', [{procs, Pid}, {print_file, Filename}, debug]),
+  redbug_start(?FUNCTION_NAME, 'receive', [{procs, Pid}]),
   Pid ! pling,
-  timer:sleep(100),
-  redbug:stop(),
-  timer:sleep(100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"pling">>,
-               get_line_seg(Filename, 2, 3)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 2, 3)).
 
-t_5_test() ->
-  Filename = "redbug5.txt",
-  {_, _, _} = redbug:start("lists:sort->time", [{print_file, Filename}, {time, 999}, debug]),
+call_time_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort->time", []),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(1100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
+               get_line_seg(Content, 2, 2)),
   ?assertEqual(<<"lists:sort/1">>,
-               get_line_seg(Filename, 3, 8)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 3, 8)).
 
-t_6_test() ->
-  Filename = "redbug6.txt",
-  {_, _, _} = redbug:start("lists:sort->count", [{print_file, Filename}, {time, 999}, debug]),
+call_count_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort->count", []),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(1100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
+               get_line_seg(Content, 2, 2)),
   ?assertEqual(<<"lists:sort/1">>,
-               get_line_seg(Filename, 3, 4)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 3, 4)).
 
-t_7_test() ->
-  Os = [blocking, {time, 999}, {msgs, 1000}, arity, debug],
-  {timeout, Msgs} = redbug:start(["erlang:demonitor", "erlang:monitor"], Os),
+blocking_test() ->
+  Options = [blocking, arity, {time, 499}, debug],
+  {timeout, Msgs} = redbug:start(["erlang:demonitor", "erlang:monitor"], Options),
   ?assertEqual([{erlang, demonitor, 1},
                 {erlang, monitor, 2}],
                [MFA || {call, {MFA, _}, _, _} <- Msgs]).
 
-t_8_test() ->
-  {_, _, _} = redbug:start("lists:sort->return", [{file, "foo"}, {time, 999}, debug]),
+trace_file_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort->return", [{file, "foo"}]),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(1100),
+  redbug_normal_stop(),
   {2, Msgs} = replay_trc:go("foo0.trc", fun(E, A) -> [E]++A end, []),
+  maybe_delete("foo0.trc"),
   ?assertEqual(sort,
                e(2, e(4, e(1, Msgs)))),
   ?assertEqual(sort,
-               e(2, e(4, e(2, Msgs)))),
-  maybe_delete("foo0.trc").
+               e(2, e(4, e(2, Msgs)))).
 
-t_9_test() ->
-  Filename = "redbug9.txt",
-  Options = [{print_file, Filename}, {time, 999}, {print_return, false}],
-  {_, _, _} = redbug:start("lists:sort->return", [debug|Options]),
+no_return_test() ->
+  redbug_start(?FUNCTION_NAME, "lists:sort->return", [{print_return, false}]),
   [1, 2, 3] = lists:sort([3, 2, 1]),
-  timer:sleep(1100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"lists:sort([3,2,1])">>,
-               get_line_seg(Filename, 2, 2)),
+               get_line_seg(Content, 2, 2)),
   ?assertEqual([<<"lists:sort/1">>, <<"->">>, <<"'...'">>],
-               get_line_seg(Filename, 4, 2, 4)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 4, 2, 4)).
 
-t_10_test() ->
-  Filename = "redbug10.txt",
-  Options = [{print_file, Filename}, {time, 999}],
-  {_, _, _} = redbug:start("redbug_eunit:ipl()->return", [debug|Options]),
+improper_list_test() ->
+  redbug_start(?FUNCTION_NAME, "redbug_eunit:ipl()->return", []),
   ipl(),
-  timer:sleep(1100),
-  maybe_show(Filename),
+  redbug_normal_stop(),
+  Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"redbug_eunit:ipl()">>,
-               get_line_seg(Filename, 2, 2)),
+               get_line_seg(Content, 2, 2)),
   ?assertEqual([<<"redbug_eunit:ipl/0">>, <<"->">>, <<"[a,b|c]">>],
-               get_line_seg(Filename, 4, 2, 4)),
-  maybe_delete(Filename).
+               get_line_seg(Content, 4, 2, 4)).
 
 %% test printing of improper lists
 ipl() -> [a, b|c].
@@ -205,9 +163,9 @@ loct_stripped_full_module_return_test() ->
 
 loct_stripped_non_arity_count_test() ->
   M = load_stripped_module(),
-  redbug_start(?FUNCTION_NAME, "stripped_mod:local_fun->count", [{time, 999}]),
+  redbug_start(?FUNCTION_NAME, "stripped_mod:local_fun->count", []),
   M:exported_fun(5),
-  redbug_timeout_stop(),
+  redbug_normal_stop(),
   unload_module(M),
   Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"stripped_mod:local_fun(5)">>,
@@ -217,9 +175,9 @@ loct_stripped_non_arity_count_test() ->
 
 loct_stripped_full_module_count_test() ->
   M = load_stripped_module(),
-  redbug_start(?FUNCTION_NAME, "stripped_mod->count", [{time, 999}]),
+  redbug_start(?FUNCTION_NAME, "stripped_mod->count", []),
   M:exported_fun(5),
-  redbug_timeout_stop(),
+  redbug_normal_stop(),
   unload_module(M),
   Content = redbug_output(?FUNCTION_NAME),
   ?assertEqual(<<"stripped_mod:local_fun(5)">>,
@@ -285,9 +243,6 @@ redbug_normal_stop() ->
   redbug:stop(),
   timer:sleep(100).
 
-redbug_timeout_stop() ->
-  timer:sleep(1100).
-
 write_beam(M, Bin) ->
   TmpDir = filename:dirname(code:which(redbug_eunit)),
   TmpFile = filename:join(TmpDir, beam_filename(M)),
@@ -308,22 +263,15 @@ redbug_output(Name) ->
   maybe_delete(Filename),
   Content.
 
-maybe_show(Filename) when is_list(Filename) ->
-  [io:fwrite("~p~n", [read_file(Filename)]) || in_shell()];
 maybe_show(Content) ->
   [io:fwrite("~p~n", [Content]) || in_shell()].
 
-lines(Filename) when is_list(Filename) ->
-  length(read_file(Filename));
 lines(Content) ->
   length(Content).
 
 get_line_seg(Content, Line, Seg) ->
   hd(get_line_seg(Content, Line, Seg, Seg)).
 
--define(is_string(A), A >= 0, A =< 256).
-get_line_seg([A|_] = Filename, Line, SegF, SegL) when ?is_string(A) ->
-  [e(S, e(Line, read_file(Filename))) || S <- lists:seq(SegF, SegL)];
 get_line_seg(Content, Line, SegF, SegL) ->
   [e(S, e(Line, Content)) || S <- lists:seq(SegF, SegL)].
 
